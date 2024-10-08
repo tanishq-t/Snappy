4efd1962899d9fde2bc3d06c92f412d72865251c
Tanishq Tyagi <tyagitanishq45@gmail.com>
2024-10-08 22:46:22 +0530

    initial commit

---
+++ Frontend/src/utils/APIroutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:5000"
+export const registerRoute  = `${host}/api/v1/users/register`;
+export const loginRoute  = `${host}/api/v1/users/login`;
+export const setAvatarRoute  = `${host}/api/v1/users/setAvatar`;
+export const allUsersRoute  = `${host}/api/v1/users/allUsers`;
+export const sendMessageRoute  = `${host}/api/v1/messages/addmsg`;
+export const recieveMessageRoute  = `${host}/api/v1/messages/getallmsg`;
+export const logoutRoute  = `${host}/api/v1/users/logout`;
