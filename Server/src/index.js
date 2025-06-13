0edf4824984be46e2a4b65caacd125ce9037328a
Tanishq Tyagi <149141752+tanishq-t@users.noreply.github.com>
2025-06-13 13:01:08 +0530

    Update index.js

--- Server/src/index.js
+++ Server/src/index.js
@@ -13,7 +13,7 @@ connectDB()
     const server = http.createServer(app);
     const io = new Server(server, {
       cors: {
-        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+        origin: process.env.CORS_ORIGIN || "http://localhost:5173/",
         credentials: true,
       },
     });
